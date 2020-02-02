6f78c233da286b6fe1a340fd2ac9219f77423654
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-02-03 01:48:09 +0900

    とりあえず出力 spaceのIdとAccess Tokenは一旦隠した

--- contentful/src/App.js
+++ contentful/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import * as contentful from "contentful";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  client = contentful.createClient({
+    space: "<Space ID>",
+    accessToken: "<Content Delivery API - access token>"
+  });
+
+  state = {
+    items: []
+  };
+
+  componentDidMount = () => {
+    this.client.getEntries().then(response => {
+      this.setState({
+        items: response.items
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.items.map(item => (
+          <li>{item.fields.title} - {item.fields.bodyText}</li>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
